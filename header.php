ae7da65df86eb87ad266e4ff2f4cdee7437afd39
jack <bonardi14@gmail.com>
2020-03-11 12:08:43 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <link rel="stylesheet" href="/style/styles.css">
+</head>
+<header>
+    <div id='title'>
+        <h1>Nom du site</h1>
+    </div>
+    <div id="slider"></div>
+</header>
+<body>
